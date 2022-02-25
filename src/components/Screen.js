df5fa130b9aa7b4e0651e11b8978183e6c43261d
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-02-26 09:11:45 +1100

    custom components

---
+++ src/components/Screen.js
@@ -0,0 +1,9 @@
+// import { Textfit } from 'react-textfit';
+import './Screen.css';
+
+const Screen = ({ value }) => {
+    return (
+        <div className='screen' mode='single' max={70}> {value}</div>
+    )
+}
+export default Screen;
